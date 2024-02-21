89a7c4148f9a33c5d7632298dea823954eeaa037
vivek-repo01 <147979512+vivek-repo01@users.noreply.github.com>
2024-02-22 02:03:07 +0530

    New code

---
+++ div_by_7.py
@@ -0,0 +1,10 @@
+num = 0
+while (True):
+
+    num = int(input("Enter the number"))
+    if (num%7==0):
+        print("Num div by 7")
+       
+    else:
+        print("Num not div by 7")
+        break
