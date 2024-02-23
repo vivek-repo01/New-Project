d8da7232a05f3980ad86e604c5e34fd7d5b42ce9
vivek-repo01 <147979512+vivek-repo01@users.noreply.github.com>
2024-02-24 02:02:18 +0530

    Adding a new file

---
+++ city_monuments.py
@@ -0,0 +1,11 @@
+num = (input("Enter d city of ur choice frm d list: Delhi, Agra, Jaipur")).capitalize()
+
+if num=="Delhi":
+    print("Red Fort")
+elif num=="Agra":
+    print("TJ")
+elif num=="Jaipur":
+    print("Jal Mahal")
+else:
+    print(num)
+
